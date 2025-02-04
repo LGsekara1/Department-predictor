0cd29fee3417a4d6dd607ab30d246fdca61d1c86
Lakindu Gunasekara <lakindugunasekara123@gmail.com>
2025-02-04 17:58:14 +0530

    Update scriptnew with MLmodel.js
    js script updated to access the ML model to predict department using a flask API at backend of web application (locally)

--- src/scriptnew.js
+++ src/scriptnew.js
@@ -9,19 +9,21 @@ function evaluateField(event){
     var index_str = index.slice(-1);
     var index_num = parseInt(index.slice(0,-1));
     console.log("str = "+index_str+"num = "+index_num);
-
+    
+    
     if (230000>index_num ||index_num>=240000 || !(/^[a-zA-Z]+$/.test(index_str))){
         alert("Index "+index+"is invalid !");
         return false;
     }
-
-    var GPA = parseFloat(document.getElementById("InputGPA").value);
+    const GPA = parseFloat(document.getElementById("InputGPA").value);
+    const rank = parseInt(document.getElementById("Inputrank").value);
+    
     if (0>GPA || GPA>4){
         alert("GPA: "+GPA +"is invalid!");
         return false;
     }
 
-    var rank = parseInt(document.getElementById("Inputrank").value);
+    
     
     if (1>rank || rank>1000){
         alert("Rank: "+rank+" is invalid!");
@@ -29,34 +31,45 @@ function evaluateField(event){
     }
     
     
-
-    var message = departmentPredictor(GPA,rank)
     
-    openDialog(message);
+
+    fetch("http://127.0.0.1:5000/predict",{
+        
+        method:"POST",
+        headers:{"Content-Type":'application/json'
+        },
+        body:JSON.stringify({GPA:GPA,Rank:rank})
+
+    })
+    .then(response=>response.json()) /*The response is converted to a JSON format */
+    .then(data=> {
+        if (data.error){
+            openDialog(`JS1Error:${data.error}`);
+            return;
+        
+        }
+        let resultMsg = "Top 3 predicted Departments:<br>";
+        for(const [dept,prob] of Object.entries(data)){
+            resultMsg+=`✅<b>${dept}</b>: ${(prob*100).toFixed(2)}%<br>`;
+        }
+
+        openDialog(resultMsg);
+    })
+    .catch(error=>{
+        console.error("JSError:",error);
+        openDialog("Error:"+error);
+    });
     event.preventDefault();
     return true;
     
 }
 
 
-function departmentPredictor(GPA,rank){
-    if (GPA >3.7){
-        var message =  "You can apply to any department of preference. For more details of each department check the links below."
-    }else if (GPA >=3.5){
-        var message =  "You are eligible to be selected to any department excluding ENTC."
-    }else if(GPA>=3.4){
-        var message = "You are eligible to be selected to any department excluding ENTC and BME."
-    }else{
-        var message =  "You are eligible to be selected to any department excluding ENTC, BME, CSE and EE."
-    }
-    message="Congratulations on your results! "+message;
-    return message
-}
 
 function openDialog(message){
     var dialog = document.getElementById("dialog");
     const dialogMessage = document.getElementById("customMessage");
-    dialogMessage.textContent = message;
+    dialogMessage.innerHTML = message;
     
     
     dialog.classList.add("visible");
